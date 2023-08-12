59ccc1ad460e6adfb7fd77dcba8d72bd39ef8ff4
Bos8086 <Boma.obomanu86@gmail.com>
2023-08-12 22:57:18 +0100

    Tips Screen created

--- screens/TipsPage.js
+++ screens/TipsPage.js
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const TipsPage = ({navigate}) => {
+const TipsPage = ({ navigate }) => {
   return (
-    <View>
-      <Text>TipsPage</Text>
+    <View style={styles.mainView}>
+      <View style={styles.mainHeading}>
+        <Text style={styles.headingText}>Tips</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default TipsPage
+const styles = StyleSheet.create({
+  mainButton: {
+    backgroundColor: "black",
+    marginBottom: 10,
+    marginTop: 50,
+    padding: 5,
+    width: "60%",
+    height: 100,
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  optionView: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    height: "60%",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
+    //fontFamily:'sans-serif'
+  },
+  centerView: {
+    width: "100%",
+    height: "100%",
+  },
+  mainView: {
+    backgroundColor: "lightgrey",
+    width: "100%",
+    height: "100%",
+  },
+  mainHeading: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  headingText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default TipsPage;
